bfe213adfaab9baf4e14db477d52b1e555384c75
caizhangbin <caizhangbin@gmail.com>
2024-12-06 22:56:30 -0500

    update command line

--- batch.sh
+++ batch.sh
@@ -20,7 +20,7 @@ module load bbmap/38.86
 module load quast/5.2.0
 source ~/HolmGenome/bin/activate
 
-python HolmGenome.py -i /lustre04/scratch/zhangbin/HolmGenome/data -o /lustre04/scratch/zhangbin/HolmGenome/data/output /
---trimmomatic_path $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar /
---adapters_path /lustre04/scratch/zhangbin/db/adapters.fa /
---prokka_db_path /lustre04/scratch/zhangbin/db/prokka/prokka_db /
+python HolmGenome.py -i /lustre04/scratch/zhangbin/HolmGenome/data -o /lustre04/scratch/zhangbin/HolmGenome/data/output \
+--trimmomatic_path $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar \
+--adapters_path /lustre04/scratch/zhangbin/db/adapters.fa \
+--prokka_db_path /lustre04/scratch/zhangbin/db/prokka/prokka_db 
